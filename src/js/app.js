b58308b9360ed44c1edbefa2d88ffd30d777745f
GenesisMauries <exodo_54@hotmail.com>
2018-08-24 17:51:14 -0500

    Agrega div

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+// let menu = document.getElementById('menu');
+
+// const moveMenu = () => {
+// alert('Siento tu click pero aun no hago nada :(');
+// }
+
+// menu.addEventListener('click', moveMenu);
